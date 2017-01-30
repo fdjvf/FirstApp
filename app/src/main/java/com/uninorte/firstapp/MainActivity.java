ef2e5523b423b74c03d4d058355bb6a63097d25f
Augusto Salazar <augustosalazar@uninorte.edu.co>
2017-01-30 15:46:27 -0500

    Primer push

--- app/src/main/java/com/uninorte/firstapp/MainActivity.java
+++ app/src/main/java/com/uninorte/firstapp/MainActivity.java
@@ -22,17 +22,13 @@ public class MainActivity extends AppCompatActivity {
         Float Gr2=Float.parseFloat(((EditText)findViewById(R.id.Grade2)).getText().toString());
         Float Gr3=Float.parseFloat(((EditText)findViewById(R.id.Grade3)).getText().toString());
         float Gr4=(3.5f*4f)-Gr1-Gr2-Gr3;
-if (Gr1>5 || Gr2>5 || Gr3>5)
-{
 
-
-}
         if (Gr4<=0)
         {
             Toast.makeText(this,"Felicidades no necesitas nota",Toast.LENGTH_LONG).show();
         }else if(Gr4<=5)
         {
-                Toast.makeText(this,"Lastima, necesitas:"+Float.toString(Gr4) ,Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"Lastima,necesitas:"+Float.toString(Gr4) ,Toast.LENGTH_LONG).show();
         }
         else
         {

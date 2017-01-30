db9b64ed4fb9fcf1d8a9a76a0307cad351d997d0
Augusto Salazar <augustosalazar@uninorte.edu.co>
2017-01-30 16:41:53 -0500

    Version 2.0

--- app/src/main/java/com/uninorte/firstapp/MainActivity.java
+++ app/src/main/java/com/uninorte/firstapp/MainActivity.java
@@ -1,10 +1,10 @@
 package com.uninorte.firstapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,24 +17,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void GetMinGrade(View view) {
-
-      Float Gr1=Float.parseFloat(((EditText)findViewById(R.id.Grade1)).getText().toString());
-        Float Gr2=Float.parseFloat(((EditText)findViewById(R.id.Grade2)).getText().toString());
-        Float Gr3=Float.parseFloat(((EditText)findViewById(R.id.Grade3)).getText().toString());
-        float Gr4=(3.5f*4f)-Gr1-Gr2-Gr3;
-
-        if (Gr4<=0)
+        EditText Txb1 = (EditText) findViewById(R.id.Grade1);
+        EditText Txb2 = (EditText) findViewById(R.id.Grade2);
+        EditText Txb3 = (EditText) findViewById(R.id.Grade3);
+        if (Txb1.getText().toString().isEmpty() || Txb2.getText().toString().isEmpty() || Txb3.getText().toString().isEmpty())
         {
-            Toast.makeText(this,"Felicidades no necesitas nota",Toast.LENGTH_LONG).show();
-        }else if(Gr4<=5)
-        {
-                Toast.makeText(this,"Lastima,necesitas:"+Float.toString(Gr4) ,Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Ningun campo de nota puede estar vacio", Toast.LENGTH_LONG).show();
         }
         else
         {
-            Toast.makeText(this,"No hay nada que puedas hacer" ,Toast.LENGTH_LONG).show();
-        }
+            Float Gr1 = Float.parseFloat(Txb1.getText().toString());
+            Float Gr2 = Float.parseFloat(Txb2.getText().toString());
+            Float Gr3 = Float.parseFloat(Txb3.getText().toString());
+            if (Gr1 > 5 || Gr2 > 5 || Gr3 > 5) {
+                Toast.makeText(this, "Ningun nota puede ser mayor de 5", Toast.LENGTH_LONG).show();
+                return;
+            }
+            float Gr4 = (3.5f * 4f) - Gr1 - Gr2 - Gr3;
+            TextView Mytext = (TextView) findViewById(R.id.LastGrade);
 
+            if (Gr4 <= 0) {
+                Mytext.setText("Felicidades no necesitas nota");
+
+            } else if (Gr4 <= 5) {
+                Mytext.setText("Necesitas una nota de: " + Float.toString(Gr4));
+
+            } else
+
+            {
+                Mytext.setText("No hay nada que puedas hacer");
+
+            }
+
+
+        }
 
 
 
